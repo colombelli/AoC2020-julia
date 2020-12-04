3d879ddd3e8f0b0ac6117fa6284b2ec09b16fbc2
colombelli <xcolombelli@gmail.com>
2020-12-04 00:37:50 -0300

    added day3-part3 solution

--- Day3/toboggan_trajectory.jl
+++ Day3/toboggan_trajectory.jl
@@ -96,3 +96,61 @@ function count_trees(input)
 end
 
 println("Trees found in choosen trajectory: ", count_trees(input))
+
+
+
+#=
+--- Part Two ---
+
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+    Right 1, down 1.
+    Right 3, down 1. (This is the slope you already checked.)
+    Right 5, down 1.
+    Right 7, down 1.
+    Right 1, down 2.
+
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+=#
+
+function generic_count_trees(input,move_right,move_down)
+
+    trees = 0
+    displacement = 1
+    pattern_len = length(input[1])
+
+    for i in input[move_down+1:move_down:end]
+        displacement+=move_right
+    
+        # clock arithmetic
+        index = mod(displacement,pattern_len)
+        if index==0
+            index = pattern_len 
+        end
+
+        encounter = i[index]
+        if encounter == '#'
+            trees+=1
+        end
+    end
+    return trees
+end
+
+function compute_trees_for_each_slope(input)
+
+    multiplication = 1
+    slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+    for slope in slopes
+        trees = generic_count_trees(input,slope[1],slope[2])
+        println("Trees with slope ", slope, ": ", trees)
+        multiplication *= trees
+    end
+    println("Solution: ", multiplication)
+end
+
+println()
+compute_trees_for_each_slope(input)
