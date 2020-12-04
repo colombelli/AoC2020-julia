2ee6c0f6541e1605fb17c61bbcb1ca2e4b87088a
colombelli <xcolombelli@gmail.com>
2020-12-03 23:52:26 -0300

    added solution to day2-part2

--- Day2/password_philosophy.jl
+++ Day2/password_philosophy.jl
@@ -93,3 +93,58 @@ end
 
 println(size(policy,1),", ",size(letter,1), ", ", size(pass,1))
 println("Valid passwords found: ", count_valid_passwords(policy, letter, pass))
+
+
+#=
+--- Part Two ---
+
+While it appears you validated the passwords correctly, they don't seem to be 
+what the Official Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password 
+policy rules from his old job at the sled rental place down the street! The Official 
+Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the 
+first character, 2 means the second character, and so on. (Be careful; Toboggan 
+Corporate Policies have no concept of "index zero"!) Exactly one of these positions 
+must contain the given letter. Other occurrences of the letter are irrelevant for 
+the purposes of policy enforcement.
+
+Given the same example list from above:
+
+    1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+    1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+    2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+
+How many passwords are valid according to the new interpretation of the policies?
+=#
+
+function is_valid(policy, letter, password)
+    in_fst_pos = (password[policy[1]] == letter)
+    in_snd_pos = (password[policy[2]] == letter)
+
+    if in_fst_pos
+        if in_snd_pos
+            return false
+        else
+            return true
+        end
+    elseif in_snd_pos
+        return true
+    else
+        return false
+    end
+end
+
+function count_valid_passwords(policy, letter, pass)
+    count_valid = 0
+    for i in 1:size(pass,1)
+        if is_valid(policy[i], letter[i], pass[i])
+            count_valid+=1
+        end
+    end
+    return count_valid
+end
+
+println("Valid passwords found with second policy: ", count_valid_passwords(policy, letter, pass))
