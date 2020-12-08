ba9f7a36a932ea2788257baef59613afe1981dcb
colombelli <xcolombelli@gmail.com>
2020-12-07 21:52:40 -0300

    day6-part2 solution

--- Day6/custom_customs.jl
+++ Day6/custom_customs.jl
@@ -79,3 +79,57 @@ end
 groups = read_input()
 yesses_count = count_each_group_yesses(groups)
 println("Total sum of different yesses for each group: ", yesses_count)
+
+
+
+
+#=
+--- Part Two ---
+
+As you finish the last group's customs declaration, you notice that you misread one word in the instructions:
+
+You don't need to identify the questions to which anyone answered "yes"; you need to identify the questions to which everyone answered "yes"!
+
+Using the same example as above:
+
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+
+This list represents answers from five groups:
+
+    In the first group, everyone (all 1 person) answered "yes" to 3 questions: a, b, and c.
+    In the second group, there is no question to which everyone answered "yes".
+    In the third group, everyone answered yes to only 1 question, a. Since some people did not answer "yes" to b or c, they don't count.
+    In the fourth group, everyone answered yes to only 1 question, a.
+    In the fifth group, everyone (all 1 person) answered "yes" to 1 question, b.
+
+In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+
+For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?
+=#
+
+
+function count_each_group_yesses_intersect(groups::Array{Array{String}})::Int32
+
+    count::Int32=0
+    for group in groups
+        count += length(intersect(group...))
+    end
+    return count
+end
+
+intersect_count = count_each_group_yesses_intersect(groups)
+println("Total sum of common yesses for each group: ", intersect_count)
